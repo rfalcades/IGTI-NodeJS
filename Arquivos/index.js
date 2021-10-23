275accbce892644dbc03d9518515b282b956ba15
Ricardo Falcades <ricardo.f@evencard.com.br>
2021-10-23 11:13:05 -0300

    Alterando Json

--- Arquivos/index.js
+++ Arquivos/index.js
@@ -1,12 +1,33 @@
 import { promises as fs, write } from "fs";
 
-init();
+// init();
+writeReadJson();
 
 async function init() {
     try {
         await fs.writeFile("teste.txt", "bla bla bla");
         await fs.appendFile("teste.txt", "\nteste append file");
         const data = await fs.readFile("teste.txt", "utf-8");
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function writeReadJson() {
+    try {
+        const arrayCarros = ["Gol", "Palio", "Uno"];
+        const obj = {
+            carros: arrayCarros,
+        };
+
+        await fs.writeFile("teste.json", JSON.stringify(obj));
+
+        const data = JSON.parse(await fs.readFile("teste.json"));
+        data.carros.push("Sandero");
+
+        await fs.writeFile("teste.json", JSON.stringify(data));
+
         console.log(data);
     } catch (error) {
         console.log(error);
