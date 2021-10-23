6eb587cce2408cfd726f5581105020d3099032eb
Ricardo Falcades <ricardo.f@evencard.com.br>
2021-10-23 10:57:59 -0300

    async await

--- Arquivos/index.js
+++ Arquivos/index.js
@@ -1,18 +1,14 @@
-import fs from "fs";
+import { promises as fs, write } from "fs";
 
-fs.writeFile("teste.txt", "bla bla bla", function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    // fs.appendFile("teste.txt", "")
+init();
 
-    console.log("Sucesso Write!");
-    fs.readFile("teste.txt", "utf-8", (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
+async function init() {
+    try {
+        await fs.writeFile("teste.txt", "bla bla bla");
+        await fs.appendFile("teste.txt", "\nteste append file");
+        const data = await fs.readFile("teste.txt", "utf-8");
         console.log(data);
-      }
-    });
-  }
-});
+    } catch (error) {
+        console.log(error);
+    }
+}
