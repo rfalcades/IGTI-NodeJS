93f6dde9d267313af9016fdee77686f9a5ec7dc7
Ricardo Falcades <ricardo.f@evencard.com.br>
2021-10-24 09:49:56 -0300

    Implementacao POST

--- my-bank-api/routes/account.js
+++ my-bank-api/routes/account.js
@@ -1,5 +1,26 @@
 import express from "express";
+import { promises as fs, write } from "fs";
+
+const { readFile, writeFile } = fs;
 
 const router = express.Router();
+
+router.post("/", async (req, res) => {
+    try {
+        let account = req.body;
+        const data = JSON.parse(await readFile("accounts.json"));
+
+        account = { id: data.nextId++, ...account };
+        data.accounts.push(account);
+
+        await writeFile("accounts.json", JSON.stringify(data));
+
+        res.send(account);
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+
+    res.end();
+});
 
 export default router;
