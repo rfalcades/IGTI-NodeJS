3398e1f2fe472e8b9f622cdfad3480794df98acc
Ricardo Falcades <ricardo.f@evencard.com.br>
2021-10-24 09:57:47 -0300

    Metodo GET

--- my-bank-api/index.js
+++ my-bank-api/index.js
@@ -4,6 +4,8 @@ import { promises as fs, write } from "fs";
 
 const { readFile, writeFile } = fs;
 
+global.fileName = "accounts.json";
+
 const app = express();
 app.use(express.json());
 
@@ -11,14 +13,14 @@ app.use("/account", accountsRouter);
 
 app.listen(3000, async () => {
     try {
-        await readFile("accounts.json");
+        await readFile(global.fileName);
         console.log("API Started!");
     } catch (error) {
         const initialJson = {
             nextId: 1,
             accounts: [],
         };
-        writeFile("accounts.json", JSON.stringify(initialJson))
+        writeFile(global.fileName, JSON.stringify(initialJson))
             .then(() => {
                 console.log("API Started and File Created!");
             })
--- my-bank-api/routes/account.js
+++ my-bank-api/routes/account.js
@@ -8,19 +8,27 @@ const router = express.Router();
 router.post("/", async (req, res) => {
     try {
         let account = req.body;
-        const data = JSON.parse(await readFile("accounts.json"));
+        const data = JSON.parse(await readFile(global.fileName));
 
         account = { id: data.nextId++, ...account };
         data.accounts.push(account);
 
-        await writeFile("accounts.json", JSON.stringify(data));
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
 
         res.send(account);
     } catch (error) {
         res.status(400).send({ error: error.message });
     }
+});
 
-    res.end();
+router.get("/", async (req, res) => {
+    try {
+        const data = JSON.parse(await readFile(global.fileName));
+        delete data.nextId;
+        res.send(data);
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 });
 
 export default router;
