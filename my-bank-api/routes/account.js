d994e1a1bfeeb1389361ce440cbf1979b7191f02
Ricardo Falcades <ricardo.f@evencard.com.br>
2021-10-24 10:22:33 -0300

    PUT e PATCH

--- my-bank-api/routes/account.js
+++ my-bank-api/routes/account.js
@@ -60,4 +60,36 @@ router.delete("/:id", async (req, res) => {
     }
 });
 
+router.put("/", async (req, res) => {
+    try {
+        const account = req.body;
+
+        const data = JSON.parse(await readFile(global.fileName));
+        const index = data.accounts.findIndex((a) => a.id === account.id);
+
+        data.accounts[index] = account;
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+        res.send(account);
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+});
+
+router.patch("/updateBalance", async (req, res) => {
+    try {
+        const account = req.body;
+
+        const data = JSON.parse(await readFile(global.fileName));
+        const index = data.accounts.findIndex((a) => a.id === account.id);
+
+        data.accounts[index].balance = account.balance;
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+        res.send(data.accounts[index]);
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+});
+
 export default router;
