9b4381ab91900f198d9cb30d29365790fc16a9eb
Ricardo Falcades <ricardo.f@evencard.com.br>
2021-10-27 20:16:07 -0300

    Implementação Graphql

--- my-bank-api/index.js
+++ my-bank-api/index.js
@@ -4,6 +4,9 @@ import accountsRouter from "./routes/account.routes.js";
 import { promises as fs } from "fs";
 import cors from "cors";
 import swaggerUI from "swagger-ui-express";
+import { buildSchema } from "graphql";
+import { graphqlHTTP } from "express-graphql";
+import AccountService from "./services/account.services.js";
 
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
@@ -28,12 +31,62 @@ global.logger = winston.createLogger({
     format: combine(label({ label: "my-bank-api" }), timestamp(), myFormat),
 });
 
+const schema = buildSchema(`
+     type Account {
+         id: Int
+         name: String
+         balance: Float
+     }
+
+     input AccountInput {
+         id: Int
+         name: String
+         balance: Float         
+     }
+
+     type Query {
+         getAccounts: [Account]
+         getAccount(id: Int): Account
+     }
+
+     type Mutation {
+         createAccount(account: AccountInput): Account
+         deleteAccount(id: Int): Boolean
+         updateAccount(account: AccountInput): Account
+     }
+`);
+
+const root = {
+    getAccounts: () => AccountService.getAccounts(),
+    getAccount(args) {
+        return AccountService.getAccount(args.id);
+    },
+    createAccount({ account }) {
+        return AccountService.createAccount(account);
+    },
+    deleteAccount(args) {
+        AccountService.updateAccount(account);
+    },
+    updateAccount({ account }) {
+        return AccountService.updateAccount(account);
+    },
+};
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/doc", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use("/account", accountsRouter);
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        rootValue: root,
+        graphiql: true,
+    })
+);
 
 app.listen(3000, async () => {
     try {
